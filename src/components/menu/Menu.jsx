4e7066cd5638caa0f39379e855088c81d10b18f3
Habrhammer <wsuxvnfhpquz@mail.ru>
2022-02-17 02:38:42 +0300

    added burger-menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom";
 
-const Menu = ({ menu }) => {
+const Menu = ({ menu, active, setActive }) => {
+
   console.log(menu);
   return (
     <div className="header-nav__menu menu" data-test-id="menu">
-      <nav className="menu__body">
+      <nav className={active ? "menu__body active" : "menu__body"}>
         <ul className="menu__list">
           {menu.map(({ id, name, path }) => {
             return (
               <li className="menu__item" key={id}>
-                <Link
+                <Link onClick={()=>{setActive(!active)}}
                   to={`/${path}`}
                   className="menu__link menu-item"
                   data-test-id={`menu-link-${path}`}
